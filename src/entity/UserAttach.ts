bab0770ad25b34f76d0f7cc73167c24d65d0ed77
unknown <kimokomono74@gmail.com>
2025-05-08 20:30:00 +0300

    no relation and adding creat update delete

--- src/entity/UserAttach.ts
+++ src/entity/UserAttach.ts
@@ -9,16 +9,17 @@ import {
     JoinColumn,
     OneToOne
   } from "typeorm";
-  import { User } from "./UserEntity"; // Adjust path as needed
+  // import { User } from "./UserEntity"; // Adjust path as needed
   
   @Entity()
   export class UserAttach {
     @PrimaryGeneratedColumn()
     id!: number;
   
-    @OneToOne(() => User)
-    @JoinColumn({ name: "userId" }) // Creates foreign key column 'userId'
-    user!: User;
+    // @OneToOne(() => User)
+    // @JoinColumn({ name: "userId" }) // Creates foreign key column 'userId'
+    // @Column()
+    // user!: User;
   
     @Column()
     idFront!: string;
@@ -31,5 +32,16 @@ import {
   
     @Column()
     mobileNumber!: number;
+
+    @CreateDateColumn({ type: "timestamp" })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: "timestamp" })
+    updatedAt!: Date;   
+
+    @DeleteDateColumn({ nullable: true })
+    deletedAt?: Date;
+
+
   }
   
