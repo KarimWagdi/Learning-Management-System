542663b90bfc32c957970b65daf3a07ec2f16e99
kareem wagdy <kwagdy@matnsolution.net>
2025-05-13 21:47:29 +0300

    fix-merge

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -3,7 +3,7 @@ import UserController from "../controller/UserController";
 const router = Router();
 
 router.get("/", UserController.getUser);
-router.get('/:id', UserController.getOneUser)
+router.get('/:id', UserController.getUserById)
 router.post("/", UserController.addUser);
 router.delete("/:id", UserController.deleteUser);
 router.put("/:id", UserController.updateUser);
