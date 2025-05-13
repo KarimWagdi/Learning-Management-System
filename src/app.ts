460f42eb6ba79eb39a36fde3d455e9ddfb32a923
kareem wagdy <kwagdy@matnsolution.net>
2025-05-13 21:51:16 +0300

    fix-db-essie

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,12 @@
-import  Express  from "express";
-import { AppDataSource } from"./Config/dbConfig";
-import  Router from "./routes/IndexRoute";
+import Express from "express";
+import Router from "./routes/IndexRoute";
 const app = Express();
-
 
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 
 app.use("/api", Router);
 
-
-AppDataSource.initialize()
-  .then(() => {
-    app.listen(process.env.APP_PORT, () => {
-      console.log(`Server is running on port ${process.env.APP_PORT}`);
-    });
-  })
-  .catch((err: Error) => {
-    console.error("Error during Data Source initialization", err);
-  });
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT}`);
+});
